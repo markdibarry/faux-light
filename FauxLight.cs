a378ec355200459d7491317551c124f1dc09ca2e
Mark DiBarry <markdibarry@protonmail.com>
2026-05-03 19:32:11 -0400

    Changes for Compatibility renderer bugs

--- FauxLight.cs
+++ FauxLight.cs
@@ -5,15 +5,28 @@ namespace FauxLight;
 [Tool, GlobalClass]
 public partial class FauxLight : Sprite2D
 {
-    private static readonly ShaderMaterial s_shader;
-    private static readonly GradientTexture2D s_defaultTexture;
-
     static FauxLight()
     {
         s_defaultTexture = GetDefaultTexture();
-        s_shader = new() { Shader = GD.Load<Shader>("res://shaders/faux_lights.gdshader") };
-        s_shader.SetShaderParameter(SnapToWorldUniform, true);
+        string fileName = "res://shaders/faux_lights.gdshader";
+
+        if (FileAccess.FileExists(fileName))
+        {
+            s_shader = new() { Shader = GD.Load<Shader>(fileName) };
+            s_shader.SetShaderParameter(s_snapToWorldUniform, true);
+        }
     }
+
+    private static readonly ShaderMaterial s_shader = null!;
+    private static readonly GradientTexture2D s_defaultTexture;
+    private static readonly StringName s_maskUniform = "mask";
+    private static readonly StringName s_fpsUniform = "fps";
+    private static readonly StringName s_snapToWorldUniform = "snap_to_world";
+    private static readonly StringName s_speedUniform = "speed";
+    private static readonly StringName s_quantizeSizeUniform = "quantize_size";
+    private static readonly StringName s_levelsUniform = "levels";
+    private static readonly StringName s_ditherPatternUniform = "dither_pattern";
+    private static readonly StringName s_ditherEnabledUniform = "dither_enabled";
 
     public FauxLight()
     {
@@ -22,87 +35,78 @@ public partial class FauxLight : Sprite2D
         Material ??= s_shader;
     }
 
-    private static readonly StringName MaskUniform = "mask";
-    private static readonly StringName FPSUniform = "fps";
-    private static readonly StringName SnapToWorldUniform = "snap_to_world";
-    private static readonly StringName SpeedUniform = "speed";
-    private static readonly StringName QuantizeSizeUniform = "quantize_size";
-    private static readonly StringName LevelsUniform = "levels";
-    private static readonly StringName DitherPatternUniform = "dither_pattern";
-    private static readonly StringName DitherEnabledUniform = "dither_enabled";
+    private ShaderMaterial? Shader => Material as ShaderMaterial;
 
     [Export]
     public bool Mask
     {
-        get => field;
+        get;
         set
         {
             field = value;
-            Shader?.SetShaderParameter(MaskUniform, value);
+            Shader?.SetShaderParameter(s_maskUniform, value);
         }
     }
     [Export]
     public int FPS
     {
-        get => field;
+        get;
         set
         {
             field = value;
-            Shader?.SetShaderParameter(FPSUniform, value);
+            Shader?.SetShaderParameter(s_fpsUniform, value);
         }
     } = 10;
     [Export(PropertyHint.Range, "0.0, 5.0")]
     public float Speed
     {
-        get => field;
+        get;
         set
         {
             field = value;
-            SetInstanceShaderParameter(SpeedUniform, value);
+            SetInstanceShaderParameter(s_speedUniform, value);
         }
     } = 0.25f;
     [Export]
     public bool Dither
     {
-        get => field;
+        get;
         set
         {
             field = value;
-            Shader?.SetShaderParameter(DitherEnabledUniform, value);
+            Shader?.SetShaderParameter(s_ditherEnabledUniform, value);
         }
     }
     [Export(PropertyHint.Range, "0,1000")]
     public int QuantizeSize
     {
-        get => field;
+        get;
         set
         {
             field = value;
-            Shader?.SetShaderParameter(QuantizeSizeUniform, value);
+            Shader?.SetShaderParameter(s_quantizeSizeUniform, value);
         }
     } = 1;
     [Export(PropertyHint.Range, "2,16")]
     public int Levels
     {
-        get => field;
+        get;
         set
         {
             field = value;
-            Shader?.SetShaderParameter(LevelsUniform, value);
+            Shader?.SetShaderParameter(s_levelsUniform, value);
         }
     } = 2;
     [Export(PropertyHint.Range, "0,3,1")]
     public int Pattern
     {
-        get => field;
+        get;
         set
         {
             field = value;
-            Shader?.SetShaderParameter(DitherPatternUniform, value);
+            Shader?.SetShaderParameter(s_ditherPatternUniform, value);
         }
     } = 1;
-
-    private ShaderMaterial? Shader => Material as ShaderMaterial;
 
     /// <summary>
     /// Gets a default Gradient texture.
